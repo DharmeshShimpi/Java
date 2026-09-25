1e56e1ba89f6dd117c14b12cb61c5d36805ed46f
Dharmesh Shimpi <dharmeshshimpi7@gmail.com>
2026-09-25 13:37:35 +0530

    new(java): Wrapper Class

---
+++ WrapperClass.java
@@ -0,0 +1,16 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.142;
+        String e = "Dharmesh";
+
+
+        System.out.println(a.booleanValue());
+
+        // Autoboxing: converting a primitive into it's wrapper object automatically
+        // Unboxing: converting a wrapper object into a primitive automatically
+        
+    }
+}
